da0f0a7ade9b2fe37e860185100915e871c69307
betulbektas1 <betul1279@hotmail.com>
2024-01-17 00:55:57 +0300

    Initial commit

---
+++ NTierProject.UI/App_Start/RouteConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace NTierProject.UI
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+              name: "RecipeDetail",
+              url: "Recipe/RecipeDetail/{slug}",
+              defaults: new { controller = "Recipe", action = "RecipeDetail", slug = "" },
+              namespaces: new[] { "TarifDefterim.UI.Controllers" } // Area içerisindeki aynı isimdeki controller ile çakışmaması için kullanıyoruz.
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "NTierProject.UI.Controllers" }
+            );
+        }
+    }
+}
